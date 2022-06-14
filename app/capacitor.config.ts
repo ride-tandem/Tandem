56dabbb94b5166d557e79da19f00fa4e279e3a85
Cobey Hollier <cobey.hollier@mac.com>
2022-06-14 14:14:21 -0700

    CH: Adding capacitor-firebase auth plugin

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -1,10 +1,16 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'tandem',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "io.ionic.starter",
+  appName: "tandem",
+  webDir: "build",
+  bundledWebRuntime: false,
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ["google.com"],
+    },
+  },
 };
 
 export default config;
