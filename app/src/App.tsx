b4df95131065c9c7ce6341cfde2c5a0f8e2e20ae
Cobey Hollier <cobey.hollier@mac.com>
2022-06-18 17:33:11 -0700

    CH: Adding authentication based routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -22,25 +22,44 @@ import "@ionic/react/css/display.css";
 
 /* Theme variables */
 import "./theme/variables.css";
+import { useState } from "react";
+import { User } from "firebase/auth";
+import { auth } from "./firebase/firebase";
+import Landing from "./pages/Landing";
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/registration">
-          <Registration />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/login" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [currentUser, setCurrentUser] = useState<User | null>();
+
+  auth.onAuthStateChanged((user) => {
+    setCurrentUser(user);
+  });
+
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/registration">
+            <Registration />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route
+            exact
+            path="/landing"
+            render={() => {
+              return currentUser ? <Landing /> : <Login />;
+            }}
+          />
+          <Route exact path="/">
+            <Redirect to="/login" />
+          </Route>
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
