32e7a48d25dfc707831735bae0761cdfb2fd7af9
Doug Johnson <doug@stormtree.ca>
2022-06-19 20:52:50 -0700

    Replace standardJS with ts-standard

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
 
+/* eslint-env jest */ /* <- Tells StandardJS to recognize Jest test library */
 test('renders without crashing', () => {
   const { baseElement } = render(<App />)
   expect(baseElement).toBeDefined()
