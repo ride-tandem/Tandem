f3f893434c34284e2dfabd51dd3b5cdd98da710f
Doug Johnson <doug@stormtree.ca>
2022-07-06 23:30:37 -0700

    Implemented getUser

--- app/src/firebase/database.tsx
+++ app/src/firebase/database.tsx
@@ -1,5 +1,5 @@
 import { db } from './firebase'
-import { collection, addDoc } from 'firebase/firestore'
+import { doc, collection, addDoc, getDoc } from 'firebase/firestore'
 
 /** DATA MODEL
 - users
@@ -13,6 +13,7 @@ import { collection, addDoc } from 'firebase/firestore'
 
 /** USERS */
 const DB_COL_USERS = 'users'
+
 export interface User {
   id: string
   name: string
@@ -24,9 +25,24 @@ export const addUser = async (user: Omit<User, 'id'>): Promise<string> => {
   return await new Promise((resolve, reject) => {
     addDoc(collection(db, DB_COL_USERS), user)
       .then(val => resolve(val.id))
-      .catch(err => {
-        console.error('Error adding user: ', err)
-        reject(err)
+      .catch(err => reject(err))
+  })
+}
+
+export const getUser = async (userId: string): Promise<User> => {
+  return await new Promise((resolve, reject) => {
+    getDoc(doc(db, DB_COL_USERS, userId))
+      .then(snapshot => {
+        if (snapshot.exists()) {
+          const data = snapshot.data()
+          resolve({
+            id: userId,
+            name: data.name,
+            authProvider: data.authProvider,
+            email: data.email
+          })
+        } else { reject(new Error('User id does not exist.')) }
       })
+      .catch(err => reject(err))
   })
 }
